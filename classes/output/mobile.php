9cb2e93870b4382ef7d5ff686f88decfeb0e795d
Thomas Winkler <thomas.winkler@wunderbyte.at>
2023-07-20 16:13:28 +0200

    testrefresh

--- classes/output/mobile.php
+++ classes/output/mobile.php
@@ -292,7 +292,7 @@ class mobile {
                     'cache-view' => false
                 ],
             ],
-            'javascript' => 'setTimeout(function() { console.log("DOM is available now"); this.refreshData(); });',
+            'javascript' => 'setTimeout(function() { console.log("DOM is available now"); refreshContent(true); });',
             'otherdata' => '',
         ];
     }
