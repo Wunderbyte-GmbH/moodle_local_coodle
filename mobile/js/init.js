96dbd09b459f5b329c7deb3b4e484b7bfb7a47fc
Thomas Winkler <thomas.winkler@wunderbyte.at>
2023-08-21 16:11:13 +0200

    impressum

--- mobile/js/init.js
+++ mobile/js/init.js
@@ -13,7 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-async function syncData2() {
+
+this.syncData2 = async () => {
     console.log('test');
     const currentSiteId = CoreSites.getCurrentSiteId();
     console.warn('my sync');
