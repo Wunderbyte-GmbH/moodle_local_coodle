5c85658a14ba2c992adf7f222e07e814a6fd6fbf
Thomas Winkler <thomas.winkler@wunderbyte.at>
2023-08-22 20:37:44 +0200

    calendar

--- classes/external/get_calendar_events.php
+++ classes/external/get_calendar_events.php
@@ -78,7 +78,7 @@ class get_calendar_events extends external_api {
         $cu->load_user($params['userid']);
         $courseid = \local_coodle\advisor::get_courseid_from_advisorid((int)$cu->advisorid);
         $groups = groups_get_user_groups($courseid, $params['userid']);
-        $eventlist = calendar_get_legacy_events($params['options']['timestart'], $params['options']['timeend'],
+        $eventlist = calendar_get_legacy_events(strtotime("-1 day"), strtotime("+1 month"),
         $params['userid'], $groups[0][0], $courseid, true,
         $params['options']['ignorehidden']);
         $events = $eventlist;
