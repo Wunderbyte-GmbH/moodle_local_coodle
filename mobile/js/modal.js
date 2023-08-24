49adbae5520b9174438864336da6f4909c574d85
Thomas Winkler <thomas.winkler@wunderbyte.at>
2023-08-24 16:43:13 +0200

    test upload file

--- mobile/js/modal.js
+++ mobile/js/modal.js
@@ -1,5 +1,4 @@
 console.log('test');
 this.openModal = () => {
     this.CONTENT_OTHERDATA.showmodal = !this.CONTENT_OTHERDATA.showmodal;
-    this.AlertController.create({header: 'Hallo Welt'}).then(alert => alert.present());
 }
