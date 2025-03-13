e0d5613b0ec94f57d82fe853578e7393b608cdaa
David Bogner <davidbogner@gmail.com>
2025-03-13 12:12:45 +0100

    fixed a bug for M4.5

--- classes/external/get_calendar_events.php
+++ classes/external/get_calendar_events.php
@@ -92,7 +92,7 @@ class get_calendar_events extends external_api {
         $cu = new \local_coodle\coodle_user();
         $cu->load_user($params['userid']);
         $courseid = \local_coodle\advisor::get_courseid_from_advisorid((int)$cu->advisorid);
-        $groups = groups_get_user_groups($courseid, $params['userid']);
+        $groups = groups_get_user_groups((int) $courseid, $params['userid']);
         $eventlist = calendar_get_legacy_events(strtotime("-1 day"), strtotime("+1 month"),
         $params['userid'], $groups[0][0], $courseid, true,
         $params['options']['ignorehidden']);
